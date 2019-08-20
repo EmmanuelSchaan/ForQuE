3b4be217ea01baa490de8113c9f5376d701a1d2b
EmmanuelSchaan <emmanuel.schaan@gmail.com>
2019-08-20 11:04:18 -0700

    added output

--- driver_cmblensrec.py
+++ driver_cmblensrec.py
@@ -25,12 +25,12 @@ cmb = StageIVCMB(beam=1., noise=1., lMin=lMinT, lMaxT=lMaxT, lMaxP=lMaxP, atm=Fa
 cmbLensRec = CMBLensRec(cmb, nProc=3)
 
 # Normalizations and lensing noise autos for Q_XY
-cmbLensRec.saveNormQ(pol=True)
+#cmbLensRec.saveNormQ(pol=True)
 cmbLensRec.loadNormQ()
 cmbLensRec.plotNoiseQAuto(pol=True)
 
 # Lensing noise crosses for Q_XY and Q_WZ
-cmbLensRec.saveNoiseCov()
+#cmbLensRec.saveNoiseCov()
 cmbLensRec.loadNoiseCov()
 cmbLensRec.plotNoiseQ(pol=True)
 
@@ -39,11 +39,11 @@ cmbLensRec.plotNoiseQ(pol=True)
 # Noise power spectrum for shear and dilation
 
 # Normalizations for shear, dilation
-cmbLensRec.saveNormSD()
+#cmbLensRec.saveNormSD()
 cmbLensRec.loadNormSD()
 
 # Lensing noises for shear, dilation and QE (test)
-cmbLensRec.saveNoiseQSD()
+#cmbLensRec.saveNoiseQSD()
 cmbLensRec.loadNoiseQSD()
 cmbLensRec.plotNoiseQSD()
 
@@ -52,12 +52,12 @@ cmbLensRec.plotNoiseQSD()
 # Noise power spectrum for the multipole estimators
 
 # precompute the multipoles of the lensing response f_TT
-cmbLensRec.save_f_TT_multipoles()
+#cmbLensRec.save_f_TT_multipoles()
 cmbLensRec.load_f_TT_multipoles()
 cmbLensRec.plot_f_TT_multipoles()
 
 # precompute the lensing noise for the monopole and quadrupole estimators
-cmbLensRec.save_N_k_TT_multipoles()
+#cmbLensRec.save_N_k_TT_multipoles()
 cmbLensRec.load_N_k_TT_multipoles()
 cmbLensRec.plotNoiseMultipoles()
 
